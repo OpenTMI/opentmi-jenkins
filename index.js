913e74c6dbae7f0ce84c1496b3eacf471a916fb6
Jussi Vatjus-Anttila <jussiva@gmail.com>
2015-10-12 21:18:36 +0100

    clean a little

--- index.js
+++ index.js
@@ -4,56 +4,62 @@ var nconf = require('nconf');
 
 function AddonJenkins (app, server, io, passport){
 
-  this.name = 'jenkins addon';
-  this.description = 'Provide Jenkins integration';
-  this.listDependencies = ['jenkins-api'];
+    this.name = 'jenkins addon';
+    this.description = 'Provide Jenkins integration';
+    this.listDependencies = ['jenkins-api'];
   
-	this.register = function(){
-    var jenkins_cfg = nconf.get('jenkins')
-    if( !jenkins_cfg || !jenkins_cfg.url ) {
-      winston.error('jenkins url not configured!');
-      return;
-    }
-		var jenkins = jenkinsapi.init(nconf.get('jenkins').url);
+    var cfg = nconf.get('jenkins')
     
-    app.get('/api/v0/ci/jobs', function(req, res){
-      jenkins.all_jobs(function(err, data) {
-        if (err){
-          res.json(500, err); 
-          return winston.error(err); 
+    this.register = function(){
+        
+        if( !cfg || !cfg.url ) {
+          winston.error('jenkins url not configured!');
+          return;
         }
-        res.json(data)
-      });
-    });
-    app.get('/api/v0/ci/computers', function(req, res){
-      jenkins.computers(function(err, data) {
-        if (err){
-          res.json(500, err); 
-          return console.log(err); 
-        }
-        res.json(data)
-      });
-    });
-
-    setInterval( function(){
-      jenkins.computers(function(err, data) {
-        if (err) { return console.log(err);  }
-        console.log('JENKINS: Got jenkins computers');
-        global.pubsub.emit('jenkins.computers', data);
-      });
-    }, 10000);
-    setInterval( function(){
-      jenkins.all_jobs(function(err, data) {
-          if (err){ 
-            return winston.error(err); 
-          }
-          console.log('JENKINS: Got jenkins jobs');
-          global.pubsub.emit('jenkins.jobs', data);
+        // initialize jenkins api
+        var jenkins = jenkinsapi.init(cfg.url);
+        
+        app.get('/api/v0/ci/jobs', function(req, res){
+          jenkins.all_jobs(function(err, data) {
+            if (err){
+              res.json(500, err); 
+              return winston.error(err); 
+            }
+            res.json(data)
+          });
         });
-    }, 10000);
+        app.get('/api/v0/ci/computers', function(req, res){
+          jenkins.computers(function(err, data) {
+            if (err){
+              res.json(500, err); 
+              return console.log(err); 
+            }
+            res.json(data)
+          });
+        });
 
-	}
-  return this;
+        // check all computers every 10 seconds
+        setInterval( function(){
+          jenkins.computers(function(err, data) {
+            if (err) { return console.log(err);  }
+            console.log('JENKINS: Got jenkins computers');
+            global.pubsub.emit('jenkins.computers', data);
+          });
+        }, 10000);
+        
+        // check all jobs every 10 seconds
+        setInterval( function(){
+          jenkins.all_jobs(function(err, data) {
+              if (err){ 
+                return winston.error(err); 
+              }
+              console.log('JENKINS: Got jenkins jobs');
+              global.pubsub.emit('jenkins.jobs', data);
+            });
+        }, 10000);
+
+    }
+    return this;
 }
 
 exports = module.exports = AddonJenkins;
